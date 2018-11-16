4ebce848b543cbd0da769c58010a7908e8e87675
xwk <570312124@qq.com>
2018-11-16 23:24:31 +0800

    增加热键脚本

---
+++ Dungeons of Borne/Assets/Scripts/HotKey.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class HotKey : MonoBehaviour
+{
+
+    [MenuItem("Tools/HotKey/SetActive  &2")]
+    static void SetObjActive()
+    {
+        GameObject selectionObj = Selection.activeGameObject;
+
+        selectionObj.SetActive(!selectionObj.activeSelf);
+    }
+}
